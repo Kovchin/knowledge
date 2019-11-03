b155ffd7eb36188804442f2629f572fd61784281
kovchin <kovchin@mail.ru>
2019-11-03 22:39:00 +0700

    03.11.2019

--- JS/GuideJavaScript/Lushenko_2/unit_17/unit_17.js
+++ JS/GuideJavaScript/Lushenko_2/unit_17/unit_17.js
@@ -4,11 +4,11 @@ let a1_res = [], a2_res = [], a3_res = [], a4_res = [], b1_res = [], b6_res = []
 /* <p> Дан массив a1 = [4, 5, 6, 7, 12, 34, 56, 78, 90, 11] - с помощью map переберите массив и создайте новый массив a1_res куда добавьте элементы данного массива умноженные на 2. Действия должны запускаться при вызове функции t1.</p>  */
 
 function t1() {
-    let a1 = [4, 5, 6, 7, 12, 34, 56, 78, 90, 11];
-    let out = a1.map(elem => {
-        a1_res.push(elem * 2);
-    });
-    document.querySelector('.out-1').innerHTML = a1_res;
+	let a1 = [4, 5, 6, 7, 12, 34, 56, 78, 90, 11];
+	let out = a1.map(elem => {
+		a1_res.push(elem * 2);
+	});
+	document.querySelector('.out-1').innerHTML = a1_res;
 }
 
 document.querySelector('.b-1').onclick = t1;
@@ -18,7 +18,11 @@ document.querySelector('.b-1').onclick = t1;
 запускаться при вызове функции t2.*/
 
 function t2() {
+	let a2 = [2, 3, 4, 5, 10, 11, 12];
 
+	a2_res = a2.map(item => item ** 2);
+
+	document.querySelector('.out-2').innerHTML = a2_res;
 }
 
 document.querySelector('.b-2').onclick = t2;
@@ -28,7 +32,11 @@ document.querySelector('.b-2').onclick = t2;
 /*  Дан массив a3 = [4,"3",6,7,"12",34,"56",78,90,11] - с помощью map переберите массив и создайте новый массив a3_res куда добавьте все элементы приведенные к числу. Действия должны запускаться при вызове функции t3. */
 
 function t3() {
+	let a3 = [4, "3", 6, 7, "12", 34, "56", 78, 90, 11];
 
+	a3_res = a3.map(item => +item);
+
+	document.querySelector('.out-3').innerHTML = a3_res;
 }
 
 document.querySelector('.b-3').onclick = t3;
@@ -37,8 +45,24 @@ document.querySelector('.b-3').onclick = t3;
 // Task 4 ============================================
 /*  Следующая задача проще будет решаться через метод forEach, который мы изучим во второй части урока! Сейчас мы делаем костыль, для отработки навыков работы с map. Дан массив a4 = [4,"3",6,7,"12",34,"56",78,90,11] - с помощью map переберите массив и создайте новый массив a4_res куда добавьте только числа из массива a4. Действия должны запускаться при вызове функции t4. Действия должны запускаться при вызове функции t4.*/
 
-function t4() {
+//Действительно получается очень костыльное решение где map по сути выступает как хитрый if...
 
+function t4() {
+	let a4 = [4, "3", 6, 7, "12", 34, "56", 78, 90, 11];
+	a4_res = [];
+
+	let out = a4.map((item) => {
+
+		if (typeof (item) === 'number') {
+			a4_res.push(item);
+			return item;
+		}
+	});
+
+	console.log(a4_res);
+	console.log(out);
+
+	document.querySelector('.out-4').innerHTML = a4_res;
 }
 
 document.querySelector('.b-4').onclick = t4;
@@ -48,11 +72,20 @@ document.querySelector('.b-4').onclick = t4;
 /*  Дан массив b1 = [3, 14, 15, 92]. C помощью filter переберите массив и создайте новый b1_res, который содержит только четные числа из b1. Действия должны запускаться при вызове функции t5. */
 
 function t5() {
+	let b1 = [3, 14, 15, 92];
+
+	b1_res = b1.filter(function (item) {
+		if (item % 2 == 0) {
+			return true;
+		}
+	})
+
+	document.querySelector('.out-5').innerHTML = b1_res;
 
 }
 
 document.querySelectorAll('.b-5').forEach(elem => {
-    elem.onclick = t5;
+	elem.onclick = t5;
 });
 
 
@@ -61,7 +94,11 @@ document.querySelectorAll('.b-5').forEach(elem => {
 /*  Дан массив b6 = [3, 14, 15, 92, "6", "5", "hello", 32]. C помощью filter переберите массив b6 и создайте новый b6_res, который содержит только числа из b6. Действия должны запускаться при вызове функции t6. */
 
 function t6() {
+	let b6 = [3, 14, 15, 92, "6", "5", "hello", 32];
 
+	b6_res = b6.filter(item => typeof (item) === "number");
+
+	document.querySelector('.out-6').innerHTML = `b6_res = ${b6_res}`;
 }
 
 document.querySelector('.b-6').onclick = t6;
@@ -71,6 +108,11 @@ document.querySelector('.b-6').onclick = t6;
 /*  Дан массив b7 = ["php-7", "html", "css", 92, "6", "5", "hello", 32]. C помощью filter переберите массив b7 и создайте новый b7_res, который содержит только строки из b7, длина которых больше 3. Действия должны запускаться при вызове функции t7. */
 
 function t7() {
+	let b7 = ["php-7", "html", "css", 92, "6", "5", "hello", 32];
+
+	b7_res = b7.filter(item => (typeof (item) === 'string'));
+
+	document.querySelector('.out-7').innerHTML = `b7_res = [${b7_res}]`;
 
 }
 
@@ -79,7 +121,23 @@ document.querySelector('.b-7').onclick = t7;
 // Task 8 ============================================
 /* Дан массив b8 = [3, 14, 15, 92, "6", "5", "hello", 32]. С помощью filter, переберите массив b8 и создайте новый массив b8_res, который содержит индексы четных элементов. Действия должны запускаться при вызове функции t8 */
 
+/*
+Без return это опять же хитрый if, а так как return true в callback выдает item то ловить индексы таким способом 
+невозможно, только внутри функции через дополнительный массив, а это проще делать через обычный цикл и сравнение
+*/
+
 function t8() {
+	let b8 = [3, 14, 15, 92, "6", "5", "hello", 32];
+
+	b8_res = [];
+
+	b8.filter((item, index) => {
+		if ((item % 2 === 0) && (typeof (item) === 'number')) {
+			b8_res.push(index);
+		}
+	});
+
+	document.querySelector('.out-8').innerHTML = `b8_res = [${b8_res}]`;
 
 }
 
@@ -89,7 +147,25 @@ document.querySelector('.b-8').onclick = t8;
 // Task 9 ============================================
 /*  Дан массив b9 = [3, "hello", 4, "world", 5, "hi"]. С помощью filter, переберите массив b5 и создайте новый массив b9_num и b9_string, которые содержат первый - числа из b9, второй - строки. Задачу решить одним filter. Действия должны запускаться при вызове функции t9. */
 
+/*
+Буду очень благодарен если в качестве обратной связи вы скажете насколько корректно пользоваться filter, 
+но не напрямую с возвратом callback, а как тут частично как результат filter, а частично как хитрое условие
+*/
+
 function t9() {
+	let b9 = [3, "hello", 4, "world", 5, "hi"],
+		b9_string = [];
+
+	let b9_num = b9.filter(item => {
+		if (typeof (item) === 'string') {
+			b9_string.push(item);
+		}
+		else
+			return true;
+	});
+
+	document.querySelector('.out-9').innerHTML = `b9_num = [${b9_num}] <br>`;
+	document.querySelector('.out-9').innerHTML += `b9_string = [${b9_string}]`;
 
 }
 
@@ -100,8 +176,18 @@ document.querySelector('.b-9').onclick = t9;
 /*  Дан массив b10 = [ [1,2,3], [3,4,6], [4,5,7], [8,9,3]]. С помощью filter переберите массив и создайте новый b10_res, в который входят вложенные массивы содержащие цифру 3. Действия должны запускаться при вызове функции t10. */
 
 function t10() {
+	let b10 = [[1, 2, 3], [3, 4, 6], [4, 5, 7], [8, 9, 3]];
+	/*
+	Вот на этом моменте ОГРОМНОЕ СПАСИБО ЗА КУРС!!!
+	*/
+	b10_res = b10.filter(item => item.find(item2 => (item2 === 3)));
 
+	console.log(b10_res);
 
 }
 
 document.querySelector('.b-10').onclick = t10;
+
+/*
+Как быстро кончились задания в этом уроке (((
+*/
